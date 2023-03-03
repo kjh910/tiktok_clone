6049f304d99cd177d72b6a2aa84f1c6546b4eb3d
kjh910 <god12nom@gmail.com>
2023-03-03 22:50:11 +0900

    add onbording

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
-import 'package:tiktok_clone/features/authentication/sign_up_screen.dart';
+import 'package:tiktok_clone/features/onboarding/interests_screen.dart';
 
 void main() {
   runApp(const TiktokApp());
@@ -28,7 +28,7 @@ class TiktokApp extends StatelessWidget {
               fontWeight: FontWeight.w600,
             ),
           )),
-      home: const SignUpScreen(),
+      home: const InterestsScreen(),
       supportedLocales: const [
         Locale("en"),
         Locale("ja"),
